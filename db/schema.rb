9f1666c5ba1f1b17f5fe778e95d74852c5cb55f5
Jul Jen <juliajen.prog@gmail.com>
2018-04-11 00:13:07 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@
 ActiveRecord::Schema.define(version: 20180410233343) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.float  "balance"
+    t.string  "username"
+    t.string  "password_digest"
+    t.decimal "balance",         precision: 8, scale: 2
   end
 
 end
