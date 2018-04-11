44ef4b9b7ec95e4ae6d132c6605dc962f813d449
Jul Jen <juliajen.prog@gmail.com>
2018-04-11 00:01:58 +0000

    automatically backed up by learn

--- db/migrate/20180410233343_add_balance_to_users.rb
+++ db/migrate/20180410233343_add_balance_to_users.rb
@@ -1,5 +1,5 @@
 class AddBalanceToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :balance, :float
+    add_column :users, :balance, :decimal, :precision => 8, :scale => 2
   end
 end
