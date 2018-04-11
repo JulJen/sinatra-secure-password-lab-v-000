2020bdba84c985b26bfa52a06531baf998c2b481
Jul Jen <juliajen.prog@gmail.com>
2018-04-11 01:44:53 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,112 @@
+# require "./config/environment"
+# require "./app/models/user"
+# class ApplicationController < Sinatra::Base
+#
+#   configure do
+#     set :views, "app/views"
+#     enable :sessions
+#     set :session_secret, "password_security"
+#   end
+#
+#   get "/" do
+#     erb :index
+#   end
+#
+#   get "/signup" do
+#     erb :signup
+#   end
+#
+#   post "/signup" do
+#     #your code here
+#
+#     user = User.new(:username => params[:username], :password => params[:password], :balance => params[:balance])
+#
+#     if user.username == ""
+#       redirect "/failure"
+#     elsif user.save
+#       redirect "/login"
+#     else
+#       redirect "/failure"
+#     end
+#   end
+#
+#   # get '/account' do
+#   #   if current_user && logged_in?
+#   #   # user = User.find(session[:user_id])
+#   #     erb :account
+#   #   end
+#   # end
+#
+# #   get '/account' do
+# # binding.pry
+# #     if logged_in?
+# #       @user = current_user
+# #
+# #       erb :account
+# #     else
+# #       redirect "/failure"
+# #     end
+# #   end
+#
+#   get '/account' do
+#     if current_user && logged_in?
+#     # user = User.find(session[:user_id])
+#       erb :account
+#     end
+#   end
+#
+#
+#
+#   get "/login" do
+#     erb :login
+#   end
+#
+#   post "/login" do
+#     #your code here
+#     user = User.find_by(:username => params[:username])
+#
+#      if user && user.authenticate(params[:password])
+#          session[:user_id] = user.id
+#          redirect "/account"
+#      else
+#          redirect "/failure"
+#      end
+#    end
+#
+#    get "/deposit" do
+#      erb :deposit
+#    end
+#
+#    get "/withdrawal" do
+#      erb :withdrawal
+#    end
+#
+#   get "/failure" do
+#     erb :failure
+#   end
+#
+#   get "/logout" do
+#     session.clear
+#     redirect "/"
+#   end
+#
+#   helpers do
+#     def logged_in?
+#       !!session[:user_id]
+#     end
+#
+#     def current_user
+#       User.find(session[:user_id])
+#     end
+#
+#     # def validate
+#     #   errors.add(:balance, "should be at least 0.00") if price.nil? || price < 0.00
+#     # end
+#   end
+#
+# end
+
+
 require "./config/environment"
 require "./app/models/user"
 class ApplicationController < Sinatra::Base
@@ -19,7 +128,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     #your code here
 
-    user = User.new(:username => params[:username], :password => params[:password], :balance => params[:balance])
+    user = User.new(:username => params[:username], :password => params[:password])
 
     if user.username == ""
       redirect "/failure"
@@ -30,31 +139,12 @@ class ApplicationController < Sinatra::Base
     end
   end
 
-  # get '/account' do
-  #   if current_user && logged_in?
-  #   # user = User.find(session[:user_id])
-  #     erb :account
-  #   end
-  # end
-
-#   get '/account' do
-# binding.pry
-#     if logged_in?
-#       @user = current_user
-#
-#       erb :account
-#     else
-#       redirect "/failure"
-#     end
-#   end
-
   get '/account' do
     if current_user && logged_in?
     # user = User.find(session[:user_id])
       erb :account
     end
   end
-
 
 
   get "/login" do
@@ -71,14 +161,6 @@ class ApplicationController < Sinatra::Base
      else
          redirect "/failure"
      end
-   end
-
-   get "/deposit" do
-     erb :deposit
-   end
-
-   get "/withdrawal" do
-     erb :withdrawal
    end
 
   get "/failure" do
@@ -98,10 +180,6 @@ class ApplicationController < Sinatra::Base
     def current_user
       User.find(session[:user_id])
     end
-
-    # def validate
-    #   errors.add(:balance, "should be at least 0.00") if price.nil? || price < 0.00
-    # end
   end
 
 end
